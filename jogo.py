854bce0d9217444094427ca3549c7f399b702c30
luana956 <luana.hipolito.pereira@escola.pr.gov.br>
2026-06-18 15:08:23 +0000

    fase 2

--- jogo.py
+++ jogo.py
@@ -9,11 +9,15 @@ print("Você digitou: ", chute )
 
 chuteNumerico = int(chute)
 
+acertou = chuteNumerico == numerosecreto
+maior = chuteNumerico > numerosecreto
+menor = chuteNumerico < numerosecreto
+
+
 #se voce digitar qualque número vou verificar se acertou ou erro
 if(numerosecreto == chuteNumerico):
     print("você acertou!") 
 else:
-
     print("vovê errou!")
 
 print("fim do jogo")
